ca133d67da96fa3b84338bd8329ed5b9531e2c14
Manoj Sonawane <sonawane.manoj@gmail.com>
2018-10-04 23:45:33 +0800

    correct gettext version

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule SetLocale.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.3.4"},
-      {:gettext, "~> 0.16.1"},
+      {:gettext, "~> 0.16.0"},
       {:earmark, "~>1.2.0", only: :dev},
       {:ex_doc, "~>0.16.4", only: :dev},
       {:excoveralls, "~> 0.7.3", only: :test}
